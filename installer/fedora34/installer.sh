0cb9f1771f0c97de4fb0ff4d0475daa1d4444de4
Gustavo Watanabe <gustavo.watanabe@gmail.com>
2021-06-06 14:55:58 +0200

    Init Fedora34 installer

---
+++ installer/fedora34/installer.sh
@@ -0,0 +1,54 @@
+
+# ADDITIONAL APPS
+# Chrome
+# Terminator
+# Meld
+# GParted
+# GIMP
+
+#SSH KEYS
+ssh-keygen -t rsa -C gustavo.watanabe@gmail.com
+chmod 0700 ~/.ssh
+chmod 0600 ~/.ssh/id_rsa
+chmod 0644 ~/.ssh/id_rsa.pub
+
+# VSCODE
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf check-update
+sudo dnf install code
+
+# DOCKER
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo docker images
+
+# DOCKER-COMPOSE
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker-compose --version
+
+# PY/ENV
+sudo dnf install make gcc zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel
+curl https://pyenv.run | bash
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+exec #SHELL
+# reload terminal...
+pyenv init
+pyenv install --list
+pyenv install <version>
+pyenv global <version>
+
+# NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+# reload terminal...
+nvm ls-remote
+nvm install <version>
+
+
