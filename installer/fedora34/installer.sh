62fcdcc87ee0b4e353cebac6fb26841889baf740
Gustavo Watanabe <gustavo.watanabe@gmail.com>
2021-07-11 12:32:48 +0200

    Added docker init variables + vscode extension error workaround

--- installer/fedora34/installer.sh
+++ installer/fedora34/installer.sh
@@ -26,8 +26,10 @@ sudo dnf config-manager \
     --add-repo \
     https://download.docker.com/linux/fedora/docker-ce.repo
 sudo dnf install docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
 sudo systemctl start docker
 sudo docker images
+
 
 # DOCKER-COMPOSE
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
